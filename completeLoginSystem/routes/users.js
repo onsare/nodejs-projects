f8354a484a123adf395cc49884fa99b1e2e8cca7
denis-onsare <denisonsare@gmail.com>
2017-10-10 23:35:52 +0300

    Creates overall layout

--- completeLoginSystem/routes/users.js
+++ completeLoginSystem/routes/users.js
@@ -6,9 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-
+/*GET login page*/
 router.get('/login', function(req, res, next){
   res.render('login', { title: 'Login' });
 });
 
+router.get('/register', function(req, res, next){
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/members', function(req, res, next){
+  res.render('members', { title: 'Members Area' });
+});
 module.exports = router;
