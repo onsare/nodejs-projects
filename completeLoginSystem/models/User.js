ee32011869d0ba75f9b55fc767bdfc9b269c194b
denis-onsare <denisonsare@gmail.com>
2017-10-11 02:45:50 +0300

    Creates: Mongoose User model

---
+++ completeLoginSystem/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var User = mongoose.model('User',{
+    email:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = User;
