1d17e7cef6e5fbf84d75aca192357b37528ac671
denis-onsare <denisonsare@gmail.com>
2017-10-17 12:56:11 +0300

    Initialize dependencies

--- blog/src/app.js
+++ blog/src/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const flash = require('connect-flash');
+const path = require('path');
+
 const app = express();
 
+//setup view engine
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+
+//setup middleware dependencies
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
-
-
-
-
-
-app.listen((3000) => {
-	console.log('App listening on http://localhost:3000');
+app.listen(3000, () => {
+	console.log('App listening on http://localhost:3000')
 });
