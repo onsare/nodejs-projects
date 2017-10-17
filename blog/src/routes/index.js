cf286b8b7f8ad04cb6fe8345a4848a4ad19b095f
denis-onsare <denisonsare@gmail.com>
2017-10-17 17:36:44 +0300

    Connects the database, through monk

--- blog/src/routes/index.js
+++ blog/src/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router =  express.Router();
 
-router.get('/', function(req, res, next){
-	res.render('index', {title: 'Home'});
+var db = require('monk')("localhost/blog");
+
+var posts = db.get('posts');
+
+posts.find({},{}, function(err, posts){
+	if (err) throw "Connection to db failed";
+
+	router.get('/', function(req, res, next){
+	res.render('index', {
+		title: 'Home',
+		posts: posts
+	});
 });
+
+});
+
+
 
 router.get('/about', function(req, res, next){
 	res.render('about', {title: 'About'});
