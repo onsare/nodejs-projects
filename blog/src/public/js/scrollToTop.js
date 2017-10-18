75f0e62b0d5f4e4e6e7a0ee591e5c0196659c5c1
denis-onsare <denisonsare@gmail.com>
2017-10-18 11:07:37 +0300

    UX:  Add scroll to Top for ease of navigation

--- blog/src/public/js/scrollToTop.js
+++ blog/src/public/js/scrollToTop.js
@@ -3,13 +3,14 @@ var btn = document.getElementById('scrollTop');
 
 window.onscroll = function(){
 	
-	if(window.scrollY > 20 ){
+	if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ){
 		btn.style.display = "block";
 	}else{
 		btn.style.display = "none";
 	}
 }
 
-var scrollTop = function(){
-	window.scrollY = 0;
+var Top = function(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
